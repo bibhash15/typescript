03be782a3f587fbfbe190cbe0a9c888701f54059
Bibhash <bibhash8088@gmail.com>
2026-04-08 00:13:11 +0530

    Object ts files

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,13 @@
+// const User = {
+//     name: "hitesh",
+//     email: "hitesh@lco.dev",
+//     isAvtive: true
+// }
+let myUser = {
+    _id: "1245",
+    name: "h",
+    email: "h@h.com",
+    isActive: false,
+};
+myUser.email = "h@gmail.com";
+export {};
